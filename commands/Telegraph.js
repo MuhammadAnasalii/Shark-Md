5a8e1ab5adb94ab5844f36d66415cfa6a43cb06d
USMAN-SER <141281506+USMAN-SER@users.noreply.github.com>
2024-01-15 22:31:28 +0500

    𓆩𝐔𝐒𝐌𝐀𝐍 𝐒𝐄𝐑𓆪

--- commands/Telegraph.js
+++ commands/Telegraph.js
@@ -1,16 +1,21 @@
 
 /**
 ========================================================
-      █▄▀ █ █▄ █ ▄▀     █▄ ▄█ █▀▄
-      █ █ █ █ ▀█ ▀▄█ ▀▀ █ ▀ █ █▄▀
+   _____ _    _          _____  _  __           __  __ _____  
+  / ____| |  | |   /\   |  __ \| |/ /          |  \/  |  __ \ 
+ | (___ | |__| |  /  \  | |__) | ' /   ______  | \  / | |  | |
+  \___ \|  __  | / /\ \ |  _  /|  <   |______| | |\/| | |  | |
+  ____) | |  | |/ ____ \| | \ \| . \           | |  | | |__| |
+ |_____/|_|  |_/_/    \_\_|  \_\_|\_\          |_|  |_|_____/ 
+ 
 ========================================================
  Copyright (C) 2022.                                                                                        
  Licensed under the  GPL-3.0 License;                                                      
  You may not use this file except in compliance with the License.    
  It is supplied in the hope that it may be useful                                     
- * @project_name : KING-MD                                                                    
- * @author : naveeddogar <https://github.com/naveeddogar>   
- * @description : KING-MD ,A Multi-functional whatsapp bot.       
+ * @project_name : Shark-Md                                                                    
+ * @author : naveeddogar <https://github.com/USMAN-SER>   
+ * @description : Shark-Md ,A Multi-functional whatsapp bot.       
  * @version 1.0.1                                                                                             
  ========================================================
  **/
@@ -27,7 +32,7 @@ cmd({
     },
     async(Void, citel,text) => {
  let a = citel.quoted ? citel.quoted.text : citel.text;
-let { data } = await axios.get(`https://api.telegra.ph/createPage?access_token=d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722&title=KING-MD+Bot&author_name=naveeddogar &content=[%7B"tag":"p","children":["${a.replace(/ /g,'+')}"]%7D]&return_content=true`);
+let { data } = await axios.get(`https://api.telegra.ph/createPage?access_token=d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722&title=Shark-Md+Bot&author_name=USMAN-SER &content=[%7B"tag":"p","children":["${a.replace(/ /g,'+')}"]%7D]&return_content=true`);
 return citel.reply(`*Paste created on telegraph*\nName:-${util.format(data.result.title)} \nUrl:- ${util.format(data.result.url)}`)
     }
 );

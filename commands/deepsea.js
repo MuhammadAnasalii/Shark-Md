3dd9843d4f0773bedf400815f2218abf325ceb4e
USMAN-SER <141281506+USMAN-SER@users.noreply.github.com>
2024-01-15 23:09:45 +0500

    𓆩𝐔𝐒𝐌𝐀𝐍 𝐒𝐄𝐑𓆪

--- commands/deepsea.js
+++ commands/deepsea.js
@@ -1,22 +1,27 @@
 /**
 ========================================================
-      █▄▀ █ █▄ █ ▄▀     █▄ ▄█ █▀▄
-      █ █ █ █ ▀█ ▀▄█ ▀▀ █ ▀ █ █▄▀
+   _____ _    _          _____  _  __           __  __ _____  
+  / ____| |  | |   /\   |  __ \| |/ /          |  \/  |  __ \ 
+ | (___ | |__| |  /  \  | |__) | ' /   ______  | \  / | |  | |
+  \___ \|  __  | / /\ \ |  _  /|  <   |______| | |\/| | |  | |
+  ____) | |  | |/ ____ \| | \ \| . \           | |  | | |__| |
+ |_____/|_|  |_/_/    \_\_|  \_\_|\_\          |_|  |_|_____/ 
+ 
 ========================================================
  Copyright (C) 2022.                                                                                        
  Licensed under the  GPL-3.0 License;                                                      
  You may not use this file except in compliance with the License.    
  It is supplied in the hope that it may be useful                                     
- * @project_name : KING-MD                                                                    
- * @author : naveeddogar <https://github.com/naveeddogar>   
- * @description : KING-MD ,A Multi-functional whatsapp bot.       
+ * @project_name : Shark-Md                                                                    
+ * @author : USMAN-SER <https://github.com/USMAN-SER>   
+ * @description : Shark-Md ,A Multi-functional whatsapp bot.       
  * @version 1.0.1                                                                                             
  ========================================================
  **/
 
  const { tlang,cmd,Config } = require('../lib')
  const maker = require('mumaker')
- let cap = `*_GENERATE BY KING-MD_*`
+ let cap = `*_GENERATE BY SHARK-MD*`
      //---------------------------------------------------------------------------
  cmd({ pattern: "deepsea", category: "textpro", desc: "Some text to image feature with various styles." }, async(Void, citel, text) => {
          if (!text) return citel.reply('_Need text._')
@@ -363,7 +368,7 @@
      )
      //---------------------------------------------------------------------------
  cmd({
-             pattern: "1917",
+             pattern: "Sunset",
              category: "textpro",
              desc: "Some text to image feature with various styles."
          },

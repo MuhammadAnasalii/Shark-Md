f83c6143d08c44699d2f7253b0f56e9c972c46de
USMAN-SER <141281506+USMAN-SER@users.noreply.github.com>
2024-01-15 23:07:47 +0500

    𓆩𝐔𝐒𝐌𝐀𝐍 𝐒𝐄𝐑𓆪

--- commands/converter.js
+++ commands/converter.js
@@ -1,15 +1,20 @@
 /**
 ========================================================
-      █▄▀ █ █▄ █ ▄▀     █▄ ▄█ █▀▄
-      █ █ █ █ ▀█ ▀▄█ ▀▀ █ ▀ █ █▄▀
+   _____ _    _          _____  _  __           __  __ _____  
+  / ____| |  | |   /\   |  __ \| |/ /          |  \/  |  __ \ 
+ | (___ | |__| |  /  \  | |__) | ' /   ______  | \  / | |  | |
+  \___ \|  __  | / /\ \ |  _  /|  <   |______| | |\/| | |  | |
+  ____) | |  | |/ ____ \| | \ \| . \           | |  | | |__| |
+ |_____/|_|  |_/_/    \_\_|  \_\_|\_\          |_|  |_|_____/ 
+ 
 ========================================================
  Copyright (C) 2022.                                                                                        
  Licensed under the  GPL-3.0 License;                                                      
  You may not use this file except in compliance with the License.    
  It is supplied in the hope that it may be useful                                     
- * @project_name : KING-MD                                                                    
- * @author : naveeddogar <https://github.com/naveeddogar>   
- * @description : KING-MD ,A Multi-functional whatsapp bot.       
+ * @project_name : Shark-Md                                                                    
+ * @author : USMAN-SER <https://github.com/USMAN-SER>   
+ * @description : Shark-Md ,A Multi-functional whatsapp bot.       
  * @version 1.0.1                                                                                             
  ========================================================
  **/
